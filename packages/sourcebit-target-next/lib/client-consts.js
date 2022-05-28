7521dcc974356db6cd3a2ecdfa84b36582c7b670
Aaron R Miller <aaron@stackbit.com>
2022-05-27 19:59:06 -0700

    use latest sourcebit from npm

--- packages/sourcebit-target-next/lib/client-consts.js
+++
@@ -1,3 +0,0 @@
-module.exports.DEFAULT_LIVE_UPDATE_PORT = 8088;
-module.exports.LIVE_UPDATE_EVENT_NAME = 'props_changed';
-module.exports.LIVE_UPDATE_NAMESPACE = '/nextjs-live-updates';
--- packages/sourcebit-target-next/lib/consts.js
+++
@@ -1,3 +0,0 @@
-const path = require('path');
-
-module.exports.DEFAULT_FILE_CACHE_PATH = path.join(process.cwd(), '.sourcebit-nextjs-cache.json');
