a5bb1196a42305796690c55159db3552f2a4fc9f
Aaron R Miller <aaron@stackbit.com>
2022-05-31 11:52:41 -0700

    switch to npm

--- bin/dev.js
+++ bin/dev.js
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 
+require('dotenv').config();
+
 const open = require('open');
 const { exec } = require('child_process');
 
-const command = 'npm run stackbit dev';
+const managementToken = process.env.CONTENTFUL_ACCESS_TOKEN || process.argv[2];
+const previewKey = process.env.CONTENTFUL_PREVIEW_TOKEN || process.argv[3];
+const spaceId = process.env.CONTENTFUL_SPACE_ID || process.argv[4];
+
+if (!managementToken || !previewKey || !spaceId) {
+    console.error('Invalid arguments.\n\nUsage:\n./dev.js <managementToken> <previewToken> <spaceId>\n');
+    process.exit(1);
+}
+
+const command = `npm run stackbit dev --contentful-access-token ${managementToken} --contentful-preview-key=${previewKey} --contentful-space-id=${spaceId}`;
 
 const devProcess = exec(command, (err) => {
     console.error(`Failed to execute ${command}`, err);
